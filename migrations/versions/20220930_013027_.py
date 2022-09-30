c7f684d370a85976637111686045479cc4007742
Francisco Palacios <Fpalacios926@gmail.com>
2022-09-30 01:32:44 -0700

    redid tables

--- migrations/versions/20220930_013027_.py
+++ migrations/versions/20220930_013027_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: b15cbfb52000
+Revision ID: 86b17044af6b
 Revises: 
-Create Date: 2022-09-30 00:59:34.507991
+Create Date: 2022-09-30 01:30:27.159450
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b15cbfb52000'
+revision = '86b17044af6b'
 down_revision = None
 branch_labels = None
 depends_on = None
