8a20ed928649ab147db661036b3c9e0851e95255
Francisco Palacios <Fpalacios926@gmail.com>
2022-10-02 18:02:52 -0700

    sorta got the structure of the splash page set up

--- react-app/src/components/Splashpage.js
+++ react-app/src/components/Splashpage.js
@@ -1,14 +1,29 @@
 import { NavLink } from "react-router-dom";
-
+import './Splashpage.css'
 function Splashpage() {
 
     return (
-        <div style={{ display: "flex", justifyContent: 'space-between' }}>
-            <div>THIS IS THE SPLASH PAGE</div>
-            <div>
-                <NavLink to="/login">Log-In</NavLink>
-                <NavLink to="/sign-up">Sign up</NavLink>
+        <div className="splash-container">
+            <div className="navbar-container">
+                <div className="right-nav-container">
+                    <div className="logo-container">
+                        <NavLink className="title-splash" to='/'>Help!</NavLink>
+                        <img className="burger-splash" src="/static/images/logos/icons8-hamburger-48.png"></img>
+                    </div>
+                </div>
+                <div className="left-nav-container">
+                    <div className="button-container">
+                        <div className="ls-button-style" >
+                            <NavLink className='login-button' to="/login">Log In</NavLink>
+                        </div>
+                        <div className="ls-button-style">
+                            <NavLink className='sign-up-button' to="/sign-up">Sign up</NavLink>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div className="middle-container">Middle Part</div>
+            <div className="bottom-container">BottomPart</div>
         </div>
     )
 }
