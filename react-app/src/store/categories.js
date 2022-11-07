805618e94dfe1968f1de1cabdf077d58b40e2a0f
Francisco Palacios <Fpalacios926@gmail.com>
2022-11-06 23:57:33 -0800

    started reducer

---
+++ react-app/src/store/categories.js
@@ -0,0 +1,10 @@
+//constants
+const GET_CATEGORIES = 'category/GET_CATEGORIES'
+const GET_ONE_BUSI_CATEGORIES = 'category/GET_ONE_BUSI_CATEGORIES'
+const ADD_CATEGORIES = 'category/ADD_CATEGORIES'
+const REMOVE_CATEGORIES = 'category/REMOVE_CATEGORIES'
+
+
+const getCategories = () => ({
+    type: GET_CATEGORIES
+})
