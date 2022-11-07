0ef199735a68f36371a7e4c85532bf0250fd6164
Francisco Palacios <Fpalacios926@gmail.com>
2022-11-07 01:00:28 -0800

    got get all categories working

--- react-app/src/components/Categories/Categories.js
+++ react-app/src/components/Categories/Categories.js
@@ -1,20 +1,31 @@
 import './Categories.css'
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from 'react'
+import { getAllCategories } from '../../store/categories'
+
 
 export default function Categories() {
-    const typesOfBusiness = [
-        { "type": "Burgers", "icon": "linkHere" },
-        { "type": "Pizza", "icon": "linkHere" },
-        { "type": "Italian", "icon": "linkHere" },
-        { "type": "Mexican", "icon": "linkHere" },
-        { "type": "American", "icon": "linkHere" },
-        { "type": "Japanese", "icon": "linkHere" },
-        { "type": "Chineses", "icon": "linkHere" },
-    ]
+    // const typesOfBusiness = [
+    //     { "type": "Burgers", "icon": "linkHere" },
+    //     { "type": "Pizza", "icon": "linkHere" },
+    //     { "type": "Italian", "icon": "linkHere" },
+    //     { "type": "Mexican", "icon": "linkHere" },
+    //     { "type": "American", "icon": "linkHere" },
+    //     { "type": "Japanese", "icon": "linkHere" },
+    //     { "type": "Chineses", "icon": "linkHere" },
+    // ]
+    const dispatch = useDispatch()
+    const Cate = useSelector(state => state.categories)
+    const cateArr = Object.values(Cate)
+    // console.log(cateArr)
+    useEffect(() => {
+        dispatch(getAllCategories())
+    }, [])
     return (
         <div className='entire-categories-container' >
-            {typesOfBusiness.map(business => (
-                <h2 className='categories-item'>
-                    {business.type}
+            {cateArr.map(category => (
+                <h2 key={category.id} className='categories-item'>
+                    {category.name}
                 </h2>
             ))}
         </div>
--- react-app/src/store/categories.js
+++ react-app/src/store/categories.js
@@ -5,6 +5,33 @@ const ADD_CATEGORIES = 'category/ADD_CATEGORIES'
 const REMOVE_CATEGORIES = 'category/REMOVE_CATEGORIES'
 
 
-const getCategories = () => ({
-    type: GET_CATEGORIES
+const getCategories = (categories) => ({
+    type: GET_CATEGORIES,
+    categories
 })
+export const getAllCategories = () => async (dispatch) => {
+    const response = await fetch('/api/categories')
+
+    if (response.ok) {
+        const data = await response.json()
+        dispatch(getCategories(data))
+        return data
+    }
+    return response
+}
+
+const initialState = {}
+
+export default function categoryReducer(state = initialState, action) {
+    let newState;
+    switch (action.type) {
+        case GET_CATEGORIES:
+            const allCate = {}
+            action.categories.categories.forEach(category => {
+                allCate[category.id] = category
+            })
+            return { ...allCate }
+        default:
+            return state
+    }
+}
