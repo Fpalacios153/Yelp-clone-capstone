1531a21080ae3fe5f335750715b68f92b9561500
Francisco Palacios <Fpalacios926@gmail.com>
2022-12-14 20:15:56 -0800

    got the cate search almost working

--- react-app/src/components/Business/ViewAllBusinesses.js
+++ react-app/src/components/Business/ViewAllBusinesses.js
@@ -26,13 +26,12 @@ export default function Businesses() {
 
     const businesses = Object.values(businessesList)
     console.log(businesses)
+    const [selection, setSelection] = useState([...businesses])
     console.log(selectedCate)
 
+    console.log(selection, 'select')
 
 
-    useEffect(() => {
-        dispatch(getAllBusinesses()).then(() => setIsLoaded(true))
-    }, [dispatch])
     useEffect(() => {
         async function fetchData() {
             const response = await fetch('/api/categories')
@@ -45,6 +44,35 @@ export default function Businesses() {
         fetchData()
     }, [])
 
+
+    useEffect(() => {
+        dispatch(getAllBusinesses()).then(() => setIsLoaded(true))
+
+    }, [dispatch])
+
+
+    useEffect(() => {
+        // setSelection(businesses)
+
+
+        let picked = []
+        businesses.filter(bus => {
+            if (bus.categories) {
+                // console.log('this', bus.categories)
+                bus.categories.filter(cat => {
+                    if (cat.name === selectedCate) {
+                        console.log(bus)
+                        picked.push(bus)
+                        // return
+                    }
+                })
+            }
+            setSelection(picked)
+        })
+
+    }, [selectedCate])
+
+    console.log(selection, 'select')
 
 
     return isLoaded ? (
@@ -61,10 +89,60 @@ export default function Businesses() {
                 </div>
                 <div className="search-by-category-container">
                     {categories.map(cate => (
-                        <SearchCategories cate={cate} />
+                        <SearchCategories cate={cate} businesses={businesses} selectedCate={selectedCate} setSelectedCate={setSelectedCate} setSelection={setSelection} />
                     ))}
                 </div>
-                {businesses.map((bus, idx) => (
+                {selectedCate.length > 0 ? selection?.map((bus, idx) => (
+                    <div key={bus.id} className='business-container'>
+                        <NavLink className='navlink-business-list' to={`/businesses/${bus.id}`}>
+                            <div className="business-list">
+                                <img
+                                    className="business-image"
+                                    src={bus.image}
+                                    alt={bus.name}
+                                    onError={e => { e.currentTarget.src = '/static/images/restpic/pexels-aleksandar-pasaric-3342739.jpg' }}
+
+                                />
+                                <div className="business-text-container">
+                                    <div className="business-name">
+                                        {idx + 1}.
+                                        <div style={{ paddingLeft: '5px' }}>
+                                            {bus.name}
+                                        </div>
+                                    </div>
+                                    {/* <FavoritesOnBus /> */}
+
+                                    <div className="business-average">
+                                        <AverageStarRating reviewAverage={bus.reviewAverage} />
+                                        <span style={{ fontWeight: '500', paddingLeft: '10px', paddingRight: '5px' }}>{bus.reviewAverage > 0 ? bus.reviewAverage.toFixed(1) : 0}</span>
+                                        {' '}({bus.reviewCount > 0 ? bus.reviewCount : 0} reviews)
+                                    </div>
+                                    {/* {getId(bus.id)} */}
+                                    {/* <GetCategoriesOfBusiness business={bus} /> */}
+                                    <div className="entire-category-container-view-all">
+
+                                        {bus.categories && bus.categories.map(cate => (
+                                            <div className='business-category-item-view-all' key={cate.id}>
+                                                <div className="category-button-container-view-all">{cate.name}</div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="business-review-container">
+                                        <i className="fa-regular fa-comment"></i>
+                                        {bus.reviews ? (
+                                            <span style={{ paddingLeft: '5px', color: '#6E7072' }}>"{bus.reviews[0].review.length < 150 ? bus.reviews[0].review :
+                                                `${bus.reviews[0].review.slice(0, 150)}...see more`}</span>
+
+                                        ) :
+                                            <span style={{ paddingLeft: '5px', color: '#6E7072' }}>"No Reviews yet"</span>
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </NavLink>
+
+                    </div>
+                )) : businesses.map((bus, idx) => (
                     <div key={bus.id} className='business-container'>
                         <NavLink className='navlink-business-list' to={`/businesses/${bus.id}`}>
                             <div className="business-list">
@@ -116,6 +194,7 @@ export default function Businesses() {
                     </div>
                 ))
                 }
+
             </div >
             <Footer />
         </>
--- react-app/src/components/Categories/SearchCategory.js
+++ react-app/src/components/Categories/SearchCategory.js
@@ -1,14 +1,26 @@
 import { useEffect, useState } from "react"
 
-export default function SearchCategories({ cate }) {
+export default function SearchCategories({ cate, businesses, setSelectedCate, selectedCate, setSelection }) {
     const [categories, setCategories] = useState([])
-    const [selectedCate, setSelectedCate] = useState([])
+    // const [selectedCate, setSelectedCate] = useState([])
     const [chosen, setChosen] = useState(false)
 
 
     function afterPush(cate) {
+        // if (selectedCate.length === 0) {
         setSelectedCate(cate)
         setChosen(!chosen)
+
+        // }
+        // if (selectedCate.length > 0) {
+        //     setSelection(businesses)
+
+        //     setChosen(!chosen)
+        // }
+
+        // if (chosen) {
+        //     setSelection([])
+        // }
     }
     return (
         <div>
